2cde8b89bdbb6a2fc67bff895c8fdc4c10b269bb
Mel-ZIane <80478991+Mel-ZIane@users.noreply.github.com>
2022-03-09 09:10:14 +0100

    Add files via upload

---
+++ poème.py
@@ -0,0 +1,39 @@
+
+"""   2-obtenir le dernier mot de la ligne  """
+poème = ["La Cigale ayant chanté Tout l'Été","Se trouva fort dépourvue"]
+listerime = []
+
+dico = {
+    "E": ["é", "er", "ez", "ey", "ai", "è", "és"],
+    "U": ["ou", "oun", "ouls", "oo","ue"]
+}
+""""""
+def has_son(e, son):
+    if (son in e):
+        return True
+    else:
+        return False
+
+
+""" parcourir à l'envers pour extraire le minimum de lettre qui fait un son"""
+
+def get_son (poème,dico):
+    for key, value in dico.items():
+        son = ""
+        for i in range(len(poème) - 1, 0, -1):
+            son = poème[i] + son
+            resultat = has_son(value, son)
+            print(value, resultat, "son=", son, ":")
+            if resultat == True:
+                pass
+                break
+        if resultat == True:
+            break
+    return key
+for phrase in poème:
+    print(get_son(phrase, dico))
+    key = get_son (phrase, dico)
+    listerime.append(key)
+print(listerime)
+
+
